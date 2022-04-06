4eae3efd568013f50fd0f570f7a899b1d5ebe3c3
Anastasia Volzhina <happy-heart@bk.ru>
2022-04-06 20:19:35 +0300

    исправление ошибок после ревью

--- script/index.js
+++ script/index.js
@@ -1,28 +1,30 @@
 let formEdit = document.querySelector('.profile__edit-button');
-let formSubmit = document.querySelector('.popup');
-let formCloseBtn = formSubmit.querySelector('.popup__close');
+let formOpen = document.querySelector('.popup');
+let formCloseBtn = formOpen.querySelector('.popup__close');
+let formSubmit = formOpen.querySelector('.popup__form')
 
-let nameInput = formSubmit.querySelector('.popup__input-name');
-let jobInput = formSubmit.querySelector('.popup__input-job');
+let nameInput = formSubmit.querySelector('.popup__input_name');
+let jobInput = formSubmit.querySelector('.popup__input_job');
 let profileName = document.querySelector('.profile__user-name');
 let profileDescription = document.querySelector('.profile__description');
 let formSaveBtn = formSubmit.querySelector('.popup__save');
-let formElement = formSubmit.querySelector('.popup__content');
+let formElement = formOpen.querySelector('.popup__content');
 
 
 function togglePopup () {
-    formSubmit.classList.toggle ('popup_opened');
+    formOpen.classList.toggle ('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileDescription.textContent;
 }
-function formSubmitHandler (){
-    formSubmit.classList.toggle ('popup_opened');
+
+function formSubmitHandler (evt){
+    evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
+    togglePopup ();
 }
 
 
 formEdit.addEventListener('click', togglePopup); 
 formCloseBtn.addEventListener ('click',togglePopup);
-formSaveBtn.addEventListener('click',formSubmitHandler);
 formElement.addEventListener('submit', formSubmitHandler); 
